b06d5a9581dd1b665bea0333553f55bb472e9e8b
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2022-11-14 23:38:23 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+/* import logo from './logo.svg';
+import './App.css';*/
 
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Container from "./components/Container"
+
+/*
 function App() {
   return (
     <div className="App">
@@ -20,6 +26,27 @@ function App() {
       </header>
     </div>
   );
+}
+*/
+
+/*
+ * const n = []
+for (let i = 0; i < 1; i++){
+  n.push(
+  <div> <h1> hi </h1> </div>
+	  )
+}
+*/
+
+
+function App() {
+  return (
+    <div className="outer--container">
+	  <Header />
+	  <Container />
+	  <Footer />
+    </div>
+  )
 }
 
 export default App;
---
+++ src/components/TableView.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function SidePane(props) {
+  return (
+    <div className="table--view">
+      <div className="rules--buttons">
+	  <button>add column +</button>
+	  <button>add row +</button>
+	  <button>add rule +</button>
+      </div>
+      No tables created yet ...
+    </div>
+  )
+};
