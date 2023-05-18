df120e9416279a818050921ec0a7954fe0b233a7
Iyusuf40 <96367786+Iyusuf40@users.noreply.github.com>
2023-05-18 17:09:59 +0000

    on table modification: place name at the top

--- frontend/src/components/SidePane.js
+++ frontend/src/components/SidePane.js
@@ -1,5 +1,20 @@
 import React from 'react';
 
+function setCurrentTableToFirstPos(tablesList, currentTable) {
+  if (!tablesList.length) return
+  let indexOfCurrTable = null
+  for (let i = 0; i < tablesList.length; i++) {
+    if (tablesList[i].key === currentTable) {
+      indexOfCurrTable = i
+      break
+    }
+  }
+  if (!indexOfCurrTable) return
+  let save = tablesList[0]
+  tablesList[0] = tablesList[indexOfCurrTable]
+  tablesList[indexOfCurrTable] = save
+}
+
 export default function SidePane(props) {
   // console.log(props.records.tables)
   const { tables } = props.records;
@@ -20,6 +35,9 @@ export default function SidePane(props) {
       </h3>,
     );
   }
+
+  setCurrentTableToFirstPos(tableList, currentTable)
+
   return (
     <div className="side--pane">
       <button onClick={props.createTableBtnClicked}>
